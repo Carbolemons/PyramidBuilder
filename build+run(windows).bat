826dcbf6b0a1d926dc0a63b47bd49934d89b2e65
Carbolemons <34587311+Carbolemons@users.noreply.github.com>
2021-10-13 13:09:53 -0500

    Remove JRE binaries, add readme, add gitignore

--- build+run(windows).bat
+++ build+run(windows).bat
@@ -1,11 +1,11 @@
 @echo off
 mode 128, 48 
 type src\META-INF
-start /WAIT /B ./jdk1.8.0_301/bin/javac.exe -d ./bin @src/META-INF
+start /WAIT /B javac -d ./bin @src/META-INF
 echo.
 echo ^>^> Compiled! If no errors have been presented // Press any button to continue.
 pause > nul
 cd ./bin > nul
 cls > nul
-start /WAIT /B ../jdk1.8.0_301/bin/java.exe pyramidbuilder.PyramidBuilder
+start /WAIT /B java pyramidbuilder.PyramidBuilder
 pause
